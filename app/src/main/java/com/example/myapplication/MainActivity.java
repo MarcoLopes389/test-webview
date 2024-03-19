61b3d9e7461c9e05d70695b07b2fa701554e4f3e
Marco Lopes <marco.lopes.araujo@gmail.com>
2024-03-19 15:32:15 -0300

    working push notification

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.GeolocationPermissions;
 import android.webkit.JavascriptInterface;
@@ -12,6 +15,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
@@ -28,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = findViewById(R.id.web);
 
-        webView.loadUrl("https://discord.com/channels/@me");
+        webView.loadUrl("https://593a-170-150-253-132.ngrok-free.app"); // Substituir com link da página própria
+        webView.getSettings().setUserAgentString("Test");
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDatabaseEnabled(true);
@@ -44,17 +49,29 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @JavascriptInterface
-            public void showToast(String message) {
-                String CHANNEL_ID = "test";
+            public void showToast(String title, String message) {
+                String CHANNEL_ID = "10000001";
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Nome", importance);
+                    channel.setDescription("Descrição");
+                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(channel);
+                }
+
                 Notification notification = new NotificationCompat.Builder(this.mContext, CHANNEL_ID)
-                        .setContentTitle("Test")
-                        .setContentText("Test")
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         .build();
 
                 NotificationManagerCompat manager = NotificationManagerCompat.from(this.mContext);
                 if (ActivityCompat.checkSelfPermission(this.mContext, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                    return;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1002);
+                    }
                 }
                 manager.notify(1, notification);
             }
